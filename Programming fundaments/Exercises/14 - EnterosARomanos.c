69c78366121cb06d4acb955efca567d9a24cda1b
Jelp200 <jorge.pena0516@gmail.com>
2022-04-16 09:23:55 -0500

    :croissant: [ADD Ejercicios

---
+++ Programming fundaments/Exercises/14 - EnterosARomanos.c
@@ -0,0 +1,62 @@
+/*
+    EJERCICIO 14
+
+Que el usuario digite dos numeros y el programa diga cual de estos dos numeros es el menor.
+*/
+
+// Directivas de procesador
+#include <stdio.h>
+
+// Programa principal
+int main(){
+
+    int n, M, C, D, U;
+
+    puts("Digite el numero a convertir: "); scanf("%i", &n);
+
+    U = n%10; n /= 10;
+    D = n%10; n /= 10;
+    C = n%10; n /= 10;
+    M = n%10; n /= 10;
+
+    switch(M){
+		case 1: printf("M");break;
+		case 2: printf("MM");break;
+		case 3: printf("MMM");break;
+	}
+	switch(C){
+		case 1: printf("C");break;
+		case 2: printf("CC");break;
+		case 3: printf("CCC");break;
+		case 4: printf("CD");break;
+		case 5: printf("D");break;
+		case 6: printf("DC");break;
+		case 7: printf("DCC");break;
+		case 8: printf("DCCC");break;
+		case 9: printf("CM");break;
+	}
+	switch(D){
+		case 1: printf("X");break;
+		case 2: printf("XX");break;
+		case 3: printf("XXX");break;
+		case 4: printf("XL");break;
+		case 5: printf("L");break;
+		case 6: printf("LX");break;
+		case 7: printf("LXX");break;
+		case 8: printf("LXXX");break;
+		case 9: printf("XC");break;
+	}
+	switch(U){
+		case 1: printf("I");break;
+		case 2: printf("II");break;
+		case 3: printf("III");break;
+		case 4: printf("IV");break;
+		case 5: printf("V");break;
+		case 6: printf("VI");break;
+		case 7: printf("VII");break;
+		case 8: printf("VIII");break;
+		case 9: printf("IX");break;
+	}
+
+    return 0;
+}
